7d6799918593e85853e4b1a82660aaa9e8fcc4c6
Tejeswini-98 <90105218+Tejeswini-98@users.noreply.github.com>
2021-09-04 22:13:13 +0530

    Add files via upload

---
+++ Command.java
@@ -0,0 +1,26 @@
+package online;
+
+
+import java.util.Scanner;
+public class Command {
+ 
+    public static void main(String[] args) {
+
+    	int x,y,sum;
+    	Scanner sc = new Scanner(System.in);
+    	System.out.print("Enter the first number:");
+    	x = sc.nextInt();
+    	System.out.print("Enter the second number:");
+    	y = sc.nextInt();
+    	sum = sum(x,y);
+    	System.out.println("the sum of two number x and y is : " + sum);
+    }
+    	
+    	public static int sum(int a,int b)
+{
+int sum = a+b;
+return sum;
+} 
+    }
+
+ 
